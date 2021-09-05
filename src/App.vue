7bd4776e99f09d1c2bf6d0b7496fcbbecdb341d9
okadaaaaaaa <okadaim@noar.co.jp>
2021-09-06 02:30:15 +0900

    commit 2

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,42 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div v-if="!signedIn">
+      <amplify-authenticator></amplify-authenticator>
+    </div>
+    <div v-if="signedIn">
+      <h3>Signed in!</h3>
+      <amplify-sign-out></amplify-sign-out>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { Auth } from 'aws-amplify'
+import { AmplifyEventBus } from 'aws-amplify-vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: 'app',
+  data () {
+    return {
+      signedIn: false
+  },
+  async beforeCreate() {
+    try {
+      await Auth.currentAuthenticatedUser()
+      this.signedIn = true
+    } catch {
+      this.signedIn = false
+    }
+    AmplifyEventBus.$on('authState', info => {
+      this.signedIn = info === 'signedIn'
+    }); 
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
